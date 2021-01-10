0d3efeae7f60b1b7347379973569ac8a0fa44234
Bharath <bharaht.janjanam98@gmail.com>
2021-01-10 17:21:48 +0530

    changed first

--- imports/ui/Info.jsx
+++ imports/ui/Info.jsx
@@ -2,19 +2,10 @@ import React from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { LinksCollection } from '../api/links';
 
-export const Info = () => {
-  const links = useTracker(() => {
-    return LinksCollection.find().fetch();
-  });
-
+export const Info = () =>
   return (
     <div>
       <h2>Learn Meteor!</h2>
-      <ul>{links.map(
-        link => <li key={link._id}>
-          <a href={link.url} target="_blank">{link.title}</a>
-        </li>
-      )}</ul>
     </div>
   );
 };
