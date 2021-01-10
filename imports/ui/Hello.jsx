ae15474b761c002168a668ff80f90862ef3957f8
bharath2212 <bharath.janjanam98@gmail.com>
2021-01-10 17:02:15 +0530

    Changed

--- imports/ui/Hello.jsx
+++ imports/ui/Hello.jsx
@@ -1,16 +1,10 @@
 import React, { useState } from 'react';
 
 export const Hello = () => {
-  const [counter, setCounter] = useState(0);
-
-  const increment = () => {
-    setCounter(counter + 1);
-  };
 
   return (
     <div>
-      <button onClick={increment}>Click Me</button>
-      <p>You've pressed the button {counter} times.</p>
+      <h1>Hello this is bharath from local</h1>
     </div>
   );
 };
