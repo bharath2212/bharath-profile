123396da2e17d179e1c748ccc57d7533a20e0415
Bharath <bharaht.janjanam98@gmail.com>
2021-01-11 10:44:55 +0530

    bharath there

--- imports/ui/Info.jsx
+++ imports/ui/Info.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { LinksCollection } from '../api/links';
 
-export const Info = () =>
+export const Info = () =>{
   return (
     <div>
       <h2>Learn Meteor!</h2>
